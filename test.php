68b7cd3293539f959dd2a6d571da7d6a3f7a2cbf
jackery0308 <61306218+jackery0308@users.noreply.github.com>
2020-06-10 14:33:47 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,15 @@
 <?php
-  echo "Here";
-  ?>
+$serverName = "jackerydb.database.windows.net";
+$connectionOptions = array{
+"Database" => "jackerydb",
+"Uid" => "jackery",
+"PwD" => "Jiashen123");
+//Establishes  the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if (!$conn)
+{
+  die("Error connection: ".sqlsrv_errors());
+  }
+  echo "Db Server: Connected!!!!";
+  }
+?>

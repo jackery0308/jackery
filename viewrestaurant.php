358389344ca38b53b69cbc70ec1faaa478a7d726
jackery0308 <61306218+jackery0308@users.noreply.github.com>
2020-06-10 14:37:19 +0800

    Update viewrestaurant.php

--- viewrestaurant.php
+++ viewrestaurant.php
@@ -1,15 +1,17 @@
 <?php
-$serverName = "jackerydb.database.windows.net";
-$connectionOptions = array{
-"Database" => "jackerydb",
-"Uid" => "jackery",
-"PwD" => "Jiashen123");
-//Establishes  the connection
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if (!$conn)
-{
-  die("Error connection: ".sqlsrv_errors());
-  }
-  echo "Db Server: Connected!!!!";
-  }
+  $serverName = "jackerydb.database.windows.net";
+
+  $connectionOptions = array(
+    "Database" => "jackerydb",
+    "Uid" => "jackery",
+    "PwD" => "Jiashen123");
+
+    //Establishes  the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if (!$conn)
+    {
+        die("Error connection: ".sqlsrv_errors());
+    }
+    echo "Db Server: Connected!!!!";
+  
 ?>

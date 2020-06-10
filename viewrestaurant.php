0966534bf44c8ae5bc81672cd744fa34ae029208
jackery0308 <61306218+jackery0308@users.noreply.github.com>
2020-06-10 14:18:28 +0800

    Create viewrestaurant.php

---
+++ viewrestaurant.php
@@ -0,0 +1,14 @@
+<?php
+$serverName = "jackerydb.database.windows.net"
+$connectionOptions = array{
+"Database" => "jackeryDb",
+"Uid" => "jackery",
+"PwD" => "Jiashen123");
+//Establishes  the connection
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if (!$conn)
+{
+  die("Error connection: ".sqlsrv_errors());
+  }
+  echo "Db Server: Connected!!!!";
+  }
